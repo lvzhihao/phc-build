0ecc1dbd8dea662a8463e466f7aead7fa7d3508d
lvzhihao <edwin.lzh@gmail.com>
2011-10-25 17:25:23 +0800

    push code

---
+++ pkgs/version.mk
@@ -0,0 +1,19 @@
+
+tmp_pkgs := /tmp/phc_build_pkgs
+tmp_ext_pkgs := \/tmp\/phc_build_pkgs
+
+gmp_pkgname := gmp-5.0.2
+
+mpfr_pkgname := mpfr-3.0.1
+
+gc_pkgname := gc6.8
+
+gcc_pkgname := gcc-4.4.6
+
+boost_version := 1.47.0
+boost_pkgname := boost_1_47_0
+
+php_pkgname := php-5.3.3
+
+phc_version := 3415
+phc_pkgname := phc-r${phc_version}
